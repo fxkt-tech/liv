55a1e29a7157cdf5560c630be79598d3094a317c
Justyer <1554694323@qq.com>
2024-06-08 00:27:38 +0800

    feat(fftool): calc

---
+++ fftool/calc.go
@@ -0,0 +1,20 @@
+package fftool
+
+import "golang.org/x/exp/constraints"
+
+// 内接
+func Inscribed[T constraints.Integer](outW, outH, w, h T) (T, T) {
+	if w*outH >= outW*h {
+		return outW, outW * h / w
+	} else {
+		return outH, outH * w / h
+	}
+}
+
+func Position[T constraints.Integer](outW, outH, w, h, xp, yp T) (T, T) {
+	if w*outH >= outW*h {
+		return 0, (outH - h) * yp / 100
+	} else {
+		return (outW - w) * xp / 100, 0
+	}
+}
